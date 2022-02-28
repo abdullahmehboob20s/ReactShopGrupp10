1ea332aa16a6e8d8628cabedb8f9e8b0cffb17a5
rollyHub <rolly.farias@medieinstitutet.se>
2022-02-28 14:28:27 +0100

    michebra

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Header from "./components/Header"
 
 
 function App() {
   return (
     <div className="App">
-      Webshop in React
+      <h1>Webshop in React</h1>
+       <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
+
 function Header() {
   return (
-    <div>Header</div>
+    <div>Hello from header</div>
   )
 }
 
