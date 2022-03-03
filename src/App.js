ad56247d47b0f32d881f185d0d3b097d65926ed9
Michael Tariao <michael.tariao@medieinstitutet.se>
2022-03-03 10:22:53 +0100

    switching over to a  new branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 import React from 'react';
 
 
 function App() {
   return (
     <div className="App">
+      
       Webshop in React
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <h1>CarShop AB</h1>
+      
+
+    </div>
   )
 }
 
