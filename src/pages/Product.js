b5a5e81af157688a5e1f3af2ce515840bc919016
Michael Tariao <michael.tariao@medieinstitutet.se>
2022-03-08 21:11:03 +0100

    1st product

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 
 function Product() {
+  const [product, setProduct] = useState({});
+  const params = useParams();
+  const fetchCarData = async () => {
+    try {
+      const response = await fetch('https://codexplained.se/cars.php' + params.id);
+      const data = await response.json();
+      console.log(data);
+
+      setProduct(data)
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchCarData();
+  }, []);
+  
   return (
-    <div>Product</div>
+    <div>
+      <h1>från Product {params.id}</h1>
+      <p>hejsan</p>
+    </div>
   )
 }
 
