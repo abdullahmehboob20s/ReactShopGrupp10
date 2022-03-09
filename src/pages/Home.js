18ace56479779bca5464bddd91e40d66c860595d
Michael Tariao <michael.tariao@medieinstitutet.se>
2022-03-09 10:13:26 +0100

    typos in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,7 @@ import home from '../components/styling/home.module.css';
 
 function Home() {
   return (
-    <div className={home.homeContainer}>
-     hejsan från Home 
+    <div className={home.homeContainer}> 
       <video className={home.video}>
         <source autoPlay muted loop src='../video/ShortFilm-Porsche.mp4' type='video/mp4' />
       </video>
